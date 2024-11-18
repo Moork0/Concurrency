efa25878085114a2b6a3691ce382b298c9f26fa7
Moorko <me@moorko.net>
2024-11-18 02:17:30 +0100

    feat: implement quicksort using threadpool

--- algorithms/include/quicksort.h
+++ algorithms/include/quicksort.h
@@ -9,7 +9,42 @@
 namespace Concurrency{
 
 template<typename T>
-std::list<T> parallelQuickSort(std::list<T> input)
+std::list<T> ManualThreadingQuickSort(std::list<T> input)
+{
+    if(input.empty())
+    {
+        return input;
+    }
+    std::list<T> result;
+    result.splice(result.begin(),input,input.begin());
+
+    T const& pivot=*result.begin();
+
+    auto divide_point=std::partition(input.begin(),input.end(),
+    [&](T const& t)
+    {
+        return t < pivot;
+
+    });
+
+    std::list<T> lower_part;
+    lower_part.splice(lower_part.end(), input, input.begin(),
+    divide_point);
+
+    std::future<std::list<T>> new_lower
+    (
+        std::async(&ManualThreadingQuickSort<T>, std::move(lower_part))
+    );
+
+    auto new_higher(ManualThreadingQuickSort(std::move(input)));
+
+    result.splice(result.end(), new_higher);
+    result.splice(result.begin(), new_lower.get());
+    return result;
+}
+
+template<typename T>
+std::list<T> ThreadPoolQuickSort(std::list<T> input, ThreadPool& thread_pool)
 {
     if(input.empty())
     {
@@ -31,16 +66,30 @@ std::list<T> parallelQuickSort(std::list<T> input)
     lower_part.splice(lower_part.end(),input,input.begin(),
     divide_point);
 
-    std::future<std::list<T>> new_lower
+    auto new_lower
     (
-
-        std::async(&parallelQuickSort<T>, std::move(lower_part))
+        thread_pool.submit([&thread_pool, &lower_part] ()
+        {
+            return ThreadPoolQuickSort<T>(std::move(lower_part), thread_pool);
+        })
     );
 
-    auto new_higher(parallelQuickSort(std::move(input)));
+    if (!new_lower.has_value()) [[unlikely]]
+    {
+        throw std::runtime_error("Submitting task to thread pool failed!");
+    }
+    std::future<std::list<T>> new_lower_fut = std::move(new_lower.value());
 
-    result.splice(result.end(),new_higher);
-    result.splice(result.begin(),new_lower.get());
+    auto new_higher(ThreadPoolQuickSort(std::move(input), thread_pool));
+
+    result.splice(result.end(), new_higher);
+
+    // Run other pending tasks while waiting for our lower part to get sorted by threadpool.
+    while (new_lower_fut.wait_for(std::chrono::seconds(0)) == std::future_status::timeout)
+    {
+        thread_pool.runPendingTask();
+    }
+    result.splice(result.begin(), new_lower_fut.get());
     return result;
 }
 
